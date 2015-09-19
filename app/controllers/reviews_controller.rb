b8b5f0036cad80b9cc9bef28120349b1645f9ed8
横浜翔馬 <yokohamashoma@yokohama.local>
2015-09-19 18:31:26 +0900

    終わり

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,5 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
 
   def new
     @restaurant = Restaurant.find(params[:restaurant_id])
@@ -10,12 +11,22 @@ class ReviewsController < ApplicationController
       restaurant_id: params[:restaurant_id],
       user_id: current_user.id,
       rate: review_params["rate"]
-      body: review_params["body"]
+      # body: review_params["body"]
       )
     if @review.save
       redirect_to restaurant_url(@review.restaurant)
     else
       render :new
     end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body)
   end
 end
