ff066222fdfead17633706d9fcfed069cf1117a7
横浜翔馬 <yokohamashoma@yokohama.local>
2015-09-19 15:33:33 +0900

    レビューコントローラーにクリエイトを追加した

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,4 +5,17 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+  def create
+    @review = Review.new(
+      restaurant_id: params[:restaurant_id],
+      user_id: current_user.id,
+      rate: review_params["rate"]
+      body: review_params["body"]
+      )
+    if @review.save
+      redirect_to restaurant_url(@review.restaurant)
+    else
+      render :new
+    end
+  end
 end
